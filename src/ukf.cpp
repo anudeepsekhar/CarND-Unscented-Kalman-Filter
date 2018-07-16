873089940a3f46818162abdcfcf40303b2f47d42
anudeepsekhar <anudeep.sekhar@gmail.com>
2018-07-16 10:55:24 +0400

    Modified ukf.cpp

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -340,60 +340,7 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
   }
 
   UpdateUKF(meas_package,Zsig,n_z);
-  //  // Mmean predicted measurement
-  // VectorXd z_pred = VectorXd(n_z);
-  // z_pred.fill(0.0);
-  // for (int i = 0; i < 2 * n_aug_ + 1; i++) {
-  //   z_pred = z_pred + weights_(i) * Zsig.col(i);
-  // }
 
-  // // Measurement covariance matrix S
-  // MatrixXd S = MatrixXd(n_z, n_z);
-  // S.fill(0.0);
-  // for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
-
-  //   //residual
-  //   VectorXd z_diff = Zsig.col(i) - z_pred;
-
-  //   S = S + weights_(i) * z_diff * z_diff.transpose();
-  // }
-
-  // // Add measurement noise covariance matrix
-  // MatrixXd R = MatrixXd(n_z, n_z);
-  // R << std_laspx_*std_laspx_, 0,
-  //      0, std_laspy_*std_laspy_;
-  // S = S + R;
-
-  // // Create matrix for cross correlation Tc
-  // MatrixXd Tc = MatrixXd(n_x_, n_z);
-
-  // // Update step
-
-  // // Calculate cross correlation matrix
-  // Tc.fill(0.0);
-  // for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
-
-  //   //residual
-  //   VectorXd z_diff = Zsig.col(i) - z_pred;
-
-  //   // state difference
-  //   VectorXd x_diff = Xsig_pred_.col(i) - x_;
-
-  //   Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
-  // }
-
-  // // Kalman gain K;
-  // MatrixXd K = Tc * S.inverse();
-
-  // //residual
-  // VectorXd z_diff = z - z_pred;
-
-  // // Calculate NIS
-  // NIS_laser_ = z_diff.transpose() * S.inverse() * z_diff;
-
-  // // Update state mean and covariance matrix
-  // x_ = x_ + K * z_diff;
-  // P_ = P_ - K*S*K.transpose();
 }
 
 /**
@@ -429,73 +376,6 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   UpdateUKF(meas_package,Zsig,n_z);
 
-  // //mean predicted measurement
-  // VectorXd z_pred = VectorXd(n_z);
-  // z_pred.fill(0.0);
-  // for (int i = 0; i < 2 * n_aug_ + 1; i++) {
-  //   z_pred = z_pred + weights_(i) * Zsig.col(i);
-  // }
-
-  // //measurement covariance matrix S
-  // MatrixXd S = MatrixXd(n_z, n_z);
-  // S.fill(0.0);
-  // for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
-  //   //residual
-  //   VectorXd z_diff = Zsig.col(i) - z_pred;
-
-  //   //angle normalization
-  //   while (z_diff(1)> M_PI) z_diff(1) -= 2.*M_PI;
-  //   while (z_diff(1)<-M_PI) z_diff(1) += 2.*M_PI;
-
-  //   S = S + weights_(i) * z_diff * z_diff.transpose();
-  // }
-
-  // //add measurement noise covariance matrix
-  // MatrixXd R = MatrixXd(n_z, n_z);
-  // R << std_radr_*std_radr_,                       0,                     0,
-  //                        0, std_radphi_*std_radphi_,                     0,
-  //                        0,                       0, std_radrd_*std_radrd_;
-  // S = S + R;
-
-  // //create matrix for cross correlation Tc
-  // MatrixXd Tc = MatrixXd(n_x_, n_z);
-
-  // //calculate cross correlation matrix
-  // Tc.fill(0.0);
-  // for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
-
-  //   //residual
-  //   VectorXd z_diff = Zsig.col(i) - z_pred;
-  //   //angle normalization
-  //   while (z_diff(1)> M_PI) z_diff(1) -= 2.*M_PI;
-  //   while (z_diff(1)<-M_PI) z_diff(1) += 2.*M_PI;
-
-  //   // state difference
-  //   VectorXd x_diff = Xsig_pred_.col(i) - x_;
-  //   //angle normalization
-  //   while (x_diff(3)> M_PI) x_diff(3) -= 2.*M_PI;
-  //   while (x_diff(3)<-M_PI) x_diff(3) += 2.*M_PI;
-
-  //   Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
-  // }
-
-  // //Kalman gain K;
-  // MatrixXd K = Tc * S.inverse();
-
-  // //residual
-  // VectorXd z_diff = z - z_pred;
-
-  // //angle normalization
-  // while (z_diff(1)> M_PI) z_diff(1) -= 2.*M_PI;
-  // while (z_diff(1)<-M_PI) z_diff(1) += 2.*M_PI;
-
-  // //calculate NIS
-  // NIS_radar_ = z_diff.transpose() * S.inverse() * z_diff;
-
-  // //update state mean and covariance matrix
-  // x_ = x_ + K * z_diff;
-  // P_ = P_ - K*S*K.transpose();
-
 }
 
 void UKF::UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z){
@@ -503,14 +383,14 @@ void UKF::UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z){
   // Extract Measurements
   VectorXd z = meas_package.raw_measurements_;
 
-  //mean predicted measurement
+  // Predicted measurement mean
   VectorXd z_pred = VectorXd(n_z);
   z_pred.fill(0.0);
   for (int i = 0; i < 2 * n_aug_ + 1; i++) {
     z_pred = z_pred + weights_(i) * Zsig.col(i);
   }
 
-  //measurement covariance matrix S
+  // Measurement covariance matrix S
   MatrixXd S = MatrixXd(n_z, n_z);
   S.fill(0.0);
   for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
@@ -534,7 +414,7 @@ void UKF::UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z){
   }
   S = S + R;
 
-  //create matrix for cross correlation Tc
+  // Create matrix for cross correlation Tc
   MatrixXd Tc = MatrixXd(n_x_, n_z);
 
 
@@ -547,7 +427,7 @@ void UKF::UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z){
       NormAng(&(z_diff(1)));
     }
 
-    // state difference
+    // State difference
     VectorXd x_diff = Xsig_pred_.col(i) - x_;
     if(meas_package.sensor_type_ == MeasurementPackage::RADAR){
       NormAng(&(x_diff(3)));
@@ -556,7 +436,7 @@ void UKF::UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z){
     Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
   }
 
-  //Kalman gain K;
+  // Kalman gain K;
   MatrixXd K = Tc * S.inverse();
 
   //residual
@@ -565,7 +445,7 @@ void UKF::UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z){
       NormAng(&(z_diff(1)));
     }
 
-  //calculate NIS
+  // Calculate NIS
   if(meas_package.sensor_type_ == MeasurementPackage::RADAR){
     NIS_radar_ = z_diff.transpose() * S.inverse() * z_diff;
   }
@@ -574,7 +454,7 @@ void UKF::UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z){
   }
 
 
-  //update state mean and covariance matrix
+  // Update state mean and covariance matrix
   x_ = x_ + K * z_diff;
   P_ = P_ - K*S*K.transpose();
 
